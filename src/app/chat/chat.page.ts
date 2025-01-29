c53a0183fadea72c9fcfb9662b517f7b47d33662
AlejandroArenasLopez <alejandroarenaslop@gmail.com>
2025-01-29 12:12:41 -0600

    chats agregados

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -8,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
-  chat: any[] = [1,2,3,4,5,6,7,8,9,0];
+  chat: any[] = [{
+    username: 'Alejandro',
+    message: 'Holaaa como estas',
+    date: '11.23',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Maria',
+    message: 'Encantada de conocerte',
+    date: '12.23',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Jose',
+    message: 'xd',
+    date: '13.23',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Zahid',
+    message: 'Gracias por conocerme',
+    date: '14.23',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  }];
 
   constructor() { }
 
