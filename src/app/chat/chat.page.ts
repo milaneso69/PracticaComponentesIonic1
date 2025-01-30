fa07300734f118e85c8f0713c57da28ff17a72a8
AlejandroArenasLopez <alejandroarenaslop@gmail.com>
2025-01-30 07:42:51 -0600

    Chats llenados

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -28,6 +28,62 @@ export class ChatPage implements OnInit {
     message: 'Gracias por conocerme',
     date: '14.23',
     avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Uziel',
+    message: 'Saca el Brawl',
+    date: '12:02',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Aaron',
+    message: 'Weeeeee',
+    date: '17:20',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Eleonor',
+    message: 'Odio la vida',
+    date: '23:12',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Fatima',
+    message: 'Turip ip ip ip turip',
+    date: '14:21',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Angel',
+    message: 'Wazaaa',
+    date: '8:12',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Kevin',
+    message: 'Mary?',
+    date: '11:02',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Perla',
+    message: 'Perre',
+    date: '13:12',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Gabo',
+    message: 'Mañana voy a traer CupCakes',
+    date: '17.23',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Ashley',
+    message: 'Wacha',
+    date: '15:23',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Arturo',
+    message: 'Metete al minecraft',
+    date: '5:45',
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+  },{
+    username: 'Messi',
+    message: 'El bogueto JIJIJIJIJ',
+    date: '4:02',
+
+    avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
   }];
 
   constructor() { }
