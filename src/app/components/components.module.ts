7824a9485be4a83b8dcb03e1f964e4deb0156f26
AlejandroArenasLopez <alejandroarenaslop@gmail.com>
2025-01-30 21:42:36 -0600

    Declaracion y importacion/exportacion de itemheader

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemChatComponent } from './item-chat/item-chat.component';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    ItemChatComponent
+    ItemChatComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    ItemChatComponent
+    ItemChatComponent,
+    HeaderComponent
   ]
 })
 export class ComponentsModule { }
