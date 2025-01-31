899f72fedbac1c8b910d108e88dc55b40364937e
AlejandroArenasLopez <alejandroarenaslop@gmail.com>
2025-01-30 20:21:57 -0600

    Declaracion de componente

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemChatComponent } from './item-chat/item-chat.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ItemChatComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ItemChatComponent
   ]
 })
 export class ComponentsModule { }
