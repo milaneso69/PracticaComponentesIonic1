4f53b37f8b366da8173bc746166b0dd37dbf9416
AlejandroArenasLopez <alejandroarenaslop@gmail.com>
2025-01-30 20:33:16 -0600

    importacion de modulo de componentes para poder implementarlo

--- src/app/chats/chats.module.ts
+++ src/app/chats/chats.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ChatsPageRoutingModule } from './chats-routing.module';
 
 import { ChatsPage } from './chats.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ChatsPageRoutingModule
+    ChatsPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ChatsPage]
 })
